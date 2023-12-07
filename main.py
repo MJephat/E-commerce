758fa9c21e9d7a246b6528e225ad62d0d8b7c5c3
MJephat <mukundijephat22@gmail.com>
2023-12-07 10:32:55 +0300

    displayCategory, ProductDescription in main.py

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.secret_key = 'udhu3982398dh7'
+UPLOAD_FOLDER = 'static/uploads'
+ALLOWED_EXTENSIONS = set(['jpeg', 'jpg', 'png', 'gif'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def getLoginDetails():
     with sqlite3.connect('database.db') as conn:
@@ -35,6 +38,60 @@ def root():
         categoryData = cur.fetchall()
     itemData = parse(itemData)
     return render_template('home.html', itemData=itemData, loggedIn=loggedIn, firstName=firstName, noOfItems=noOfItems, categoryData=categoryData)
+
+
+#add
+@app.route('/add')
+def admin():
+    with sqlite3.connect('database.db') as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT categoryId, name FROM categories")
+        categories = cur.fetchall()
+    conn.close()
+    return render_template('add.html', categories=categories)
+
+@app.route("/addItem", methods=["GET", "POST"])
+def addItem():
+    if request.method == "POST":
+        name = request.form['name']
+        price = float(request.form['price'])
+        description = request.form['description']
+        stock = int(request.form['stock'])
+        categoryId = int(request.form['category'])
+
+        #Uploading image procedure
+        image = request.files['image']
+        if image and allowed_file(image.filename):
+            filename = secure_filename(image.filename)
+            image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        imagename = filename
+        with sqlite3.connect('database.db') as conn:
+            try:
+                cur = conn.cursor()
+                cur.execute('''INSERT INTO products (name, price, description, image, stock, categoryId) VALUES (?, ?, ?, ?, ?, ?)''', (name, price, description, imagename, stock, categoryId))
+                conn.commit()
+                msg="added successfully"
+            except:
+                msg="error occured"
+                conn.rollback()
+        conn.close()
+        print(msg)
+        return redirect(url_for('root'))
+
+#display  category route
+@app.route("/displayCategory")
+def displayCategory():
+    loggedIn,firstName, noOfItems = getLoginDetails()
+    categoryId = request.args.get("categoryId")
+    with sqlite3.connect('database.db') as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT products.productId, products.name, products.price, products.image, categories.name FROM products, categories WHERE products.categoryId = categories.categoryId AND categories.categoryId = ?", (categoryId, ))
+        data = cur.fetchall()
+    conn.close()
+    categoryName =data[0][4]
+    data = parse(data)
+    return render_template('displayCategory.html', data=data, loggedIn=loggedIn, firstName=firstName, noOfItems=noOfItems, categoryName=categoryName)
+
 
 #Login form
 @app.route("/loginForm")
@@ -74,7 +131,46 @@ def logout():
     session.pop('email', None)
     return redirect(url_for('root'))
 
-#
+#updateprofile routes
+@app.route("/updateprofile", methods=["POST", "GET"])
+def updateProfile():
+    if request.method == 'POST':
+        email =request.form['email']
+        firstName = request.form['firstName']
+        lastName = request.form['lastName']
+        address1 = request.form['address1']
+        address2 = request.form['address2']
+        zipcode = request.form['zipcode']
+        city = request.form['city']
+        state = request.form['state']
+        country = request.form['country']
+        phone = request.form['phone']
+        with sqlite3.connect('database.db') as con:
+            try:
+                cur = con.cursor()
+                cur.execute('UPDATE user SET firstName = ?, lastName = ?, address1 = ?, address2 = ?, zipcode = ?, city = ?, state = ?, country = ?, phone = ?, WHERE email = ?',(firstName, lastName,address1, address2, zipcode, city, state,country,phone,email))
+
+                conn.commit()
+                msg = "Saved Successfully"
+            except:
+                conn.rollback()
+                msg = "Error occured"
+        conn.close()
+        return redirect(url_for('editProfile'))
+
+#edit profile route
+@app.route('/account/profile/edit')
+def editProfile():
+    if 'email' not in session:
+        return redirect(url_for('root'))
+    loggedIn, firstName, noOfItems = getLoginDetails()
+    with sqlite3.connect('database.db') as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT userId, email,firstName,lastName, address1,address2,zipcode,city,state,country,phone FROM users WHERE email = ?", (session['email'], ))
+        profileData = cur.fetchone()
+    conn.close()
+
+    return render_template("editProfile.html", profileData=profileData,loggedIn=loggedIn, firstName=firstName, noOfItems=noOfItems)
 
 
 #register
